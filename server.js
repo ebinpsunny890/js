2730c76f5065c0d2354335ed2ac6ccf32ec42cc4
sanal6682 <sanalss6682@gmail.com>
2025-09-02 09:26:13 +0530

    latest

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.post('/users', (req, res) => {
   db.query(
     'INSERT INTO users1 (username, email) VALUES (?, ?)',
     [req.body.username, req.body.email],
-    (err) => {
+    function (err) {
       if (err) return res.status(500).send('Database error saving user');
       res.send('User saved!');
     }
@@ -30,24 +30,24 @@ app.post('/users', (req, res) => {
 });
 
 // User login - check username and email in users1 table
-app.post('/login', (req, res) => {
-  const username=req.body.username;
-  const email=req.body.email;
+app.post('/login', function (req, res) {
+    const username = req.body.username;
+    const email = req.body.email;
 
-  db.query(
-    'SELECT * FROM users1 WHERE username = ? AND email = ?',
-    [username, email],
-    (err, results) => {
-      if (err) return res.status(500).send('Database error during login');
+    db.query(
+      'SELECT * FROM users1 WHERE username = ? AND email = ?',
+      [username, email],
+      (err, results) => {
+        if (err) return res.status(500).send('Database error during login');
 
-      if (results.length > 0) {
-        res.send(`Welcome ${username}! You are logged in.`);
-      } else {
-        res.send('Invalid username or email');
+        if (results.length > 0) {
+          res.send(`Welcome ${username}! You are logged in.`);
+        } else {
+          res.send('Invalid username or email');
+        }
       }
-    }
-  );
-});
+    );
+  });
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
